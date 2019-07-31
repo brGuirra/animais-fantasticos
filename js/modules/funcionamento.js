dc83114a6a815484304f0047991891fcb7400605
Bruno Guirra <br.guirra@gmail.com>
2019-07-31 13:17:40 -0300

    Funcionamento refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,16 +1,59 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector('[data-semana]');
-  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    // Armazena o elemento que vai conster o
+    // funcionamento da empresa.
+    this.funcionamento = document.querySelector(funcionamento);
 
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horarioAgora = dataAgora.getHours();
+    // Armazena a classe que ativa o CCS.
+    this.activeClass = activeClass;
+  }
 
-  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
-  const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
+  // Função que pega os dias e horários de
+  // funcionamento da empresa.
+  dadosFuncionamento() {
+    // Armazena os dias da semana que a emprea funciona.
+    this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
 
-  if (semanaAberto && horarioAberto) {
-    funcionamento.classList.add('aberto');
+    // Armazena o horário que a empresa está aberta.
+    this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
+  }
+
+  // Função que pega o horário e dia da semana
+  // do usuário, com base no horário de Brasília.
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+
+    // Horário de Brasíla = UTC -3;
+    this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+  }
+
+  // Funão que verifica se a empresa está
+  // aberta ou não, retorna true ou false.
+  estaAberto() {
+    const semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1;
+    const horarioAberto = (this.horarioAgora >= this.horarioSemana[0]
+      && this.horarioAgora < this.horarioSemana[1]);
+
+    return semanaAberto && horarioAberto;
+  }
+
+  // Função que adiciona a classe ativo ao elemento
+  // quando a empresa estiver aberta.
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  // Funão que inicia os métodos da classe
+  // Funcionamento.
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
+    return this;
   }
 }
