aec7e482ff324066a509c9ed154d004167546132
Bruno Guirra <br.guirra@gmail.com>
2019-07-31 12:09:13 -0300

    Dropdown menu refatorado

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,19 +1,56 @@
 import outsideClick from './outsideclick.js';
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    // Armazena os elementos que são dropdown menus.
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
 
-  function handleClick(event) {
+    // Armazena os eventos que vão adicionados,
+    // se o valor não for passado a propriedade
+    // vai ter um valor padrão.
+    if (events === undefined) this.events = ['click', 'touchstart'];
+    else this.events = events;
+
+    // Classe que ativa o CSS.
+    this.activeClass = 'ativo';
+
+    // Bind no callback do objeto.
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+
+  // Função que ativa o dropdown menu e
+  // adiciona um função que observa o
+  // clique fora dele.
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add('ativo');
-    outsideClick(this, ['click', 'touchstart'], () => {
-      this.classList.remove('ativo');
+
+    // Variável que armazena o elemento que
+    // foi selecionado para que o método
+    // outsideClick() seja aplicado com referência
+    // nele.
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    ['click', 'touchstart'].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  // Função que adiciona o evento aos
+  // dropdown menus do site.
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
-  });
+  }
+
+  // Função que inicia os métodos de
+  // dropdown menu.
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownMenusEvent();
+    }
+    return this;
+  }
 }
